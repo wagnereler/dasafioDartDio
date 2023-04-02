d5bfd8b0f1777e1709c4ed8406f8f65712240582
Wagner Couto <wagnereler@hotmail.com>
2023-04-02 10:49:19 -0300

    Commit Inicial

---
+++ bin/dasafio_dart_dio.dart
@@ -0,0 +1,5 @@
+import 'package:dasafio_dart_dio/dasafio_dart_dio.dart' as dasafio_dart_dio;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dasafio_dart_dio.calculate()}!');
+}
---
+++ test/dasafio_dart_dio_test.dart
@@ -0,0 +1,8 @@
+import 'package:dasafio_dart_dio/dasafio_dart_dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
